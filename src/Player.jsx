4307519f5a0d157bb112308976a6f2684ed2201e
Mike Wolf <mw@mike-wolf.com>
2021-02-19 19:59:21 -0500

    Responsive player

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import { Box } from "@chakra-ui/react";
+
 const Player = ({ url }) => {
-  return <ReactPlayer url={url} playing />;
+  return (
+    <Box>
+      <ReactPlayer height="100%" width="100%" url={url} playing />
+    </Box>
+  );
 };
 export default Player;
