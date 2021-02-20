8e44f603ea9f2bff2a346063a0f7560504faeb1e
Mike Wolf <mw@mike-wolf.com>
2021-02-19 20:04:24 -0500

    Reverted to not responsive

--- src/Player.jsx
+++ src/Player.jsx
@@ -5,7 +5,7 @@ import { Box } from "@chakra-ui/react";
 const Player = ({ url }) => {
   return (
     <Box>
-      <ReactPlayer height="100%" width="100%" url={url} playing />
+      <ReactPlayer url={url} playing />
     </Box>
   );
 };
